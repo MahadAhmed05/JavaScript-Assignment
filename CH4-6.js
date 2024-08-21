b75119b9e179bd41443c72ed3156e527d0134bcf
MahadAhmed05 <ahmedmahad531@gmail.com>
2024-08-21 14:38:56 +0500

    New question done

--- CH4-6.js
+++ CH4-6.js
@@ -132,3 +132,31 @@
 //3)
 // let greeting = prompt("Write Greeting")
 // console.log(greeting);
+
+//5)
+var num = parseInt(prompt("Enter A Number"));
+if (num) {
+  console.log("Table of", num);
+  console.log(`${num} x 1 = ${num * 1}`);
+  console.log(`${num} x 2 = ${num * 2}`);
+  console.log(`${num} x 3 = ${num * 3}`);
+  console.log(`${num} x 4 = ${num * 4}`);
+  console.log(`${num} x 5 = ${num * 5}`);
+  console.log(`${num} x 6 = ${num * 6}`);
+  console.log(`${num} x 7 = ${num * 7}`);
+  console.log(`${num} x 8 = ${num * 8}`);
+  console.log(`${num} x 9 = ${num * 9}`);
+  console.log(`${num} x 10 = ${num * 10} `);
+} else {
+  console.log("Table of 5");
+  console.log("5 x 1 =", 5 * 1);
+  console.log("5 x 2 =", 5 * 2);
+  console.log("5 x 3 =", 5 * 3);
+  console.log("5 x 5 =", 5 * 4);
+  console.log("5 x 5 =", 5 * 5);
+  console.log("5 x 6 =", 5 * 6);
+  console.log("5 x 7 =", 5 * 7);
+  console.log("5 x 8 =", 5 * 8);
+  console.log("5 x 9 =", 5 * 9);
+  console.log("5 x 10 =", 5 * 10);
+}
