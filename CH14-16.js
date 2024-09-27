c4b36df3bfe95336df8bfd2fdea710edc0b7e569
MahadAhmed05 <ahmedmahad531@gmail.com>
2024-09-27 15:00:51 +0500

    CH17 done

---
+++ CH14-16.js
@@ -0,0 +1,80 @@
+// 1)
+// var studentNames = [];
+
+// 2)
+// var studentNames = new Array();
+
+// 3)
+// var strings = ["mahad", "zaid"];
+
+// 4)
+// var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// 5)
+// var boolean = [true, false, false, true];
+
+// 6)
+// var mixedArr = ["mahad", 5646, true, "ali", false];
+
+// 7)
+// var qualifications = [
+//   "SSC",
+//   "HSC",
+//   "BCS",
+//   "BS",
+//   "BCOM",
+//   "MS",
+//   "M. PHIL.",
+//   "PhD",
+// ];
+// console.log(qualifications);
+
+// 8)
+// var student = ["Michael", "John", "Tony"];
+// var studentScore = [320, 230, 480];
+// console.log(
+//   `Score of ${student[0]} is ${studentScore[0]}. Percentage: ${
+//     (studentScore[0] / 500) * 100
+//   }%`
+// );
+// console.log(
+//   `Score of ${student[1]} is ${studentScore[1]}. Percentage: ${
+//     (studentScore[1] / 500) * 100
+//   }%`
+// );
+// console.log(
+//   `Score of ${student[2]} is ${studentScore[2]}. Percentage: ${
+//     (studentScore[2] / 500) * 100
+//   }%`
+// );
+
+// 9)
+// var colorsName = [];
+// var color = prompt("What color you want to add on the beginning");
+// colorsName.unshift(color);
+// console.log(colorsName);
+// var color = prompt("What color you want to add on the end")
+// colorsName.push(color)
+// console.log(colorsName);
+// var color = prompt("Add two colors in the beginning");
+// colorsName.unshift(color)
+// console.log(colorsName);
+
+// 10)
+// var studentScores = [320, 230, 480, 120];
+// console.log(studentScores);
+// var orderedScored = studentScores.sort();
+// console.log(orderedScored);
+
+// 11)
+// var citiesList = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
+// var selectedCitiesList = [];
+// selectedCitiesList.push(citiesList[2])
+// selectedCitiesList.push(citiesList[3])
+// console.log(selectedCitiesList);
+
+// 12)
+// var arr = ["This", "is", "my", "cat"];
+// var joinArr = arr.join(' ')
+// console.log(joinArr);
+
---
+++ CH17-20.js
@@ -0,0 +1,96 @@
+// 3)
+// for (var i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// 4)
+// var multiplyTable = +prompt(
+//   "Enter the numner to show its multiplication table"
+// );
+// var tableLength = +prompt("Enter the length of table");
+// console.log(`Multiplication Table of ${multiplyTable}`);
+// console.log(`Length ${tableLength}`);
+// for (var i = 1; i <= tableLength; i++) {
+//   console.log(`${multiplyTable} X ${i} = ${multiplyTable * i}`);
+// }
+
+// 5)
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+// for (var i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i]);
+// }
+// console.log(" ");
+// for (var i = 0; i < fruits.length; i++) {
+//   console.log(`Element at index ${i} is ${fruits[i]}`);
+// }
+
+// 6)
+// console.log("Counting");
+// for (var i = 0; i <= 20; i++) {
+//   console.log(i);
+// }
+
+// console.log("");
+
+// console.log("Reverse Counting");
+// for (var i = 20; i >= 0; i--) {
+//   console.log(i);
+// }
+
+// console.log("");
+
+// console.log("Even");
+// for (var i = 0; i <= 20; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+// console.log("");
+
+// console.log("Odd");
+// for (var i = 0; i <= 20; i++) {
+//   if (i % 2 !== 0) {
+//     console.log(i);
+//   }
+// }
+
+// console.log("");
+
+// console.log("Series");
+// for (var i = 0; i <= 20; i++) {
+//   if (i % 2 === 0) {
+//     console.log(`${i}k`);
+//   }
+// }
+
+// 7)
+// var bakeryItems = ["cake", "applepie", "cookie", "chips", "patties"];
+// var userItem = prompt(
+//   "Welcome to ABC Bakery. What do you want to order Sir/Maam"
+// ).toLowerCase();
+// isAvailable = false;
+// for (var i = 0; i < bakeryItems.length; i++) {
+//   if (userItem === bakeryItems[i]) {
+//     console.log(`${userItem} is available at index ${i} in our bakery`);
+//     isAvailable = true;
+//     break;
+//   }
+// }
+// if (!isAvailable) {
+//   console.log(`We are sorry. ${userItem} is not available in our bakery `);
+// }
+
+// 8)
+// var num = [24, 53, 78, 91, 12];
+// var largestNum = 0;
+// for (var i = 0; i < num.length; i++) {
+//   if (num[i] > largestNum) {
+//     largestNum = num[i];
+//   }
+// }
+// console.log(largestNum);
+
+
+
+
